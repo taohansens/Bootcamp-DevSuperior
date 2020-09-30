74b0b38875f7dd47a780d083e121c5d3762522c6
Tao Hansen <taohansens@gmail.c>
2020-09-30 00:13:57 -0300

    transform List<Category> to List<CategoryDTO>, will change to Stream().map

--- backend/src/main/java/com/taohbootcamp/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/taohbootcamp/dscatalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.taohbootcamp.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.taohbootcamp.dscatalog.dto.CategoryDTO;
 import com.taohbootcamp.dscatalog.entities.Category;
 import com.taohbootcamp.dscatalog.repositories.CategoryRepository;
 
@@ -14,7 +17,15 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		List<CategoryDTO> listDto = new ArrayList<>();
+		
+		for (Category cat : list) {
+			listDto.add(new CategoryDTO(cat));
+		}
+		return listDto;
 	}
 }
