6424e24ed61fb496f3be5eeb275d4b23d669304c
Tao Hansen <taohansens@gmail.c>
2020-09-30 19:24:13 -0300

    use map to convert entities (Category -> CategoryDTO)

--- backend/src/main/java/com/taohbootcamp/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/taohbootcamp/dscatalog/services/CategoryService.java
@@ -1,7 +1,7 @@
 package com.taohbootcamp.dscatalog.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,11 +21,6 @@ public class CategoryService {
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
 		
-		List<CategoryDTO> listDto = new ArrayList<>();
-		
-		for (Category cat : list) {
-			listDto.add(new CategoryDTO(cat));
-		}
-		return listDto;
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
