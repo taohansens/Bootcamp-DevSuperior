ce81efa7eac52d9d9ab37c3cbb0e26aaed91a4d5
Tao Hansen <taohansens@gmail.c>
2020-10-02 23:47:36 -0300

    Add Paged Request

--- backend/src/main/java/com/taohbootcamp/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/taohbootcamp/dscatalog/services/CategoryService.java
@@ -9,6 +9,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -69,4 +71,9 @@ public class CategoryService {
 		throw new DatabaseException("Integrity violation");
 	}
 }
+	@Transactional(readOnly = true)
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+	Page<Category> list = repository.findAll(pageRequest);
+	return list.map(x -> new CategoryDTO(x));
+	}
 }
