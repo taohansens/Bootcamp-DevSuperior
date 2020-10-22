ff6f701f67476cf09f8715355ba4f21eb9033f4c
Tao Hansen <taohansens@gmail.com>
2020-10-21 23:35:39 -0300

    clean code

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,36 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 
 function App() {
-    const [counter, setCounter] = useState(0);
-
-    useEffect(()=>{
-        console.log('o contador mudou de valor')
-    }, [counter]) //segundo valor dependência, useEffect.
-
     return (
-        <div className="container mt-5">
-            <button
-                className="btn btn-primary mr-5"
-                onClick={() => setCounter(counter+1)}>
-            +
-            </button>
-            <span>
-                {counter}
-            </span>
-            <button
-                className="btn btn-primary ml-5"
-                onClick={() => setCounter(counter-1)}>
-                -
-            </button>
-        
-            {counter < 5 &&
-                <p className="alert alert-danger mt-3">
-                O valor é menor que 5 </p>}
-            {counter > 5 &&
-            <p className="alert alert-success mt-3">
-            O valor é maior que 5 </p>}
-        </div>
+    <h1></h1>
     )
 }
 
