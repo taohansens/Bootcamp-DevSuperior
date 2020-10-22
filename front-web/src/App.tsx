3b3ab2dacff5622e83c31d6b3db44ab1b95e6d20
Tao Hansen <taohansens@gmail.com>
2020-10-21 23:34:09 -0300

    criado contador com conceitos do useState + useEffect

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import Alert from './Alert';
+import React, {useState, useEffect} from 'react';
 
-const App = () => {
+
+function App() {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(()=>{
+        console.log('o contador mudou de valor')
+    }, [counter]) //segundo valor dependência, useEffect.
+
     return (
         <div className="container mt-5">
-            <Alert text="Tao"/>
+            <button
+                className="btn btn-primary mr-5"
+                onClick={() => setCounter(counter+1)}>
+            +
+            </button>
+            <span>
+                {counter}
+            </span>
+            <button
+                className="btn btn-primary ml-5"
+                onClick={() => setCounter(counter-1)}>
+                -
+            </button>
+        
+            {counter < 5 &&
+                <p className="alert alert-danger mt-3">
+                O valor é menor que 5 </p>}
+            {counter > 5 &&
+            <p className="alert alert-success mt-3">
+            O valor é maior que 5 </p>}
         </div>
     )
 }
