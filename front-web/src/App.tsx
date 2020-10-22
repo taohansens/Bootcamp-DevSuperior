deefb00e9624deac291fa26acb4bc0bb82a91138
Tao Hansen <taohansens@gmail.com>
2020-10-21 23:56:01 -0300

    customizing Bootstrap

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import './core/assets/styles/custom.scss';
+import './app.scss';
 
 function App() {
     return (
-    <h1></h1>
+    <h1>ReactApp</h1>
     )
 }
 
-export default App;
+export default App; 
