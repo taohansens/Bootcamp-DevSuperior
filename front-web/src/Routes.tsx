a1456dd6f4286d329edc28caa2b528447c9cea26
Tao Hansen <taohansens@gmail.com>
2020-10-23 16:39:02 -0300

    Add Home page without button

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './core/components/Navbar';
 import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
 import Home from './pages/Home';
 
 const Routes = () => (
     <BrowserRouter>
+    <Navbar />
     <Switch>
-        <Route exact patch="/">
+        <Route patch="/" exact>
             <Home />
         </Route>
         <Route path="/catalog">
