ced41a32e597949f0b38a6141f707f0d7e4417c4
Tao Hansen <taohansens@gmail.com>
2020-11-30 19:56:18 -0300

    useState example with buttons

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import Alert from './Alert';
+import React, { useState } from 'react';
 
 const App = () => {
+    const [counter, setCounter] = useState(0);
+    
     return (
-        <div className="contaiener">
-            <Alert text="Tao Hansen" />
-            <Alert text="Usuário 1" />
-            <Alert text="Usuário 2" />
-            <Alert text="Usuário 3" />
+        <div className="container mt-5">
+            <button 
+                className="btn btn-primary mr-5"
+                onClick={() => setCounter(counter + 1)}
+            >
+                +
+            </button>
+            <span>
+                {counter}
+            </span>
+            <button
+                className="btn btn-danger ml-5"
+                onClick={() => setCounter(counter - 1)}
+            >
+                -
+            </button>
         </div>
     );
 }
