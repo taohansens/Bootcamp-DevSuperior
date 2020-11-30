36418af1617bbe73576012ee284333122d5d116a
Tao Hansen <taohansens@gmail.com>
2020-11-30 20:06:08 -0300

    Clean code

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,33 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        // Ciclo de vida
-        console.log('Componente Iniciado')
-    }, [])
-
     return (
-        <div className="container mt-5">
-            <button 
-                className="btn btn-primary mr-5"
-                onClick={() => setCounter(counter + 1)}
-            >
-                +
-            </button>
-            <span>
-                {counter}
-            </span>
-            <button
-                className="btn btn-danger ml-5"
-                onClick={() => setCounter(counter - 1)}
-            >
-                -
-            </button>
-            {counter > 5 && <h1>O valor é maior que 5.</h1>}
-            {counter <= 5 && <h1>O valor é menor ou igual a 5.</h1>}
-        </div>
+        <h1>App</h1>
     );
 }
 
