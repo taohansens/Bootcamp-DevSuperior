1d459fa5b296a42d936f6d035399fd4b2c399d7b
Tao Hansen <taohansens@gmail.com>
2020-11-30 21:38:52 -0300

    Routes ok. with NavLink.

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './core/components/Navbar';
 import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
 import Home from './pages/Home';
 
 const Routes = () => (
     <BrowserRouter>
+        <Navbar />
         <Switch>
             <Route path="/" exact>
                 <Home />
