4a8673497c533d5d12bab112aa7b77ce039f04ed
Tao Hansen <taohansens@gmail.com>
2021-02-11 22:23:33 -0300

    Finished ProductDetails and created component ProductPrice

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -9,7 +9,7 @@ const Home = () => (
             <div className="col-md-6">
                 <h1 className="text-title">Conheça o melhor <br /> catálogo de produtos</h1>
                 <p className="text-subtitle">Ajudaremos você a encontrar os melhores <br /> produtos disponíveis no mercado.</p>
-                <Link to="/catalog">
+                <Link to="/products">
                     <ButtonIcon text="inicie agora sua busca"/>
                 </Link>
             </div>
