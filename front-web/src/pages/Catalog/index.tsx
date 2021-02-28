e99268e1b02d35d4a81301cc7526357bcf9e658d
Tao Hansen <taohansens@gmail.com>
2021-02-28 18:51:55 -0300

    Added content Loader (React Skeleton)

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -3,31 +3,40 @@ import { Link } from 'react-router-dom';
 import { ProductsResponse } from '../../core/types/Product';
 import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard'
+import ProductCardLoader from './components/ProductCardLoader';
 import './styles.scss'
 
 const Catalog = () => {
 
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
-    
-    console.log(productsResponse)
+    const [isLoading, setIsLoading] = useState(false)
+
     useEffect(() => {
         const params = {
             page:0,
             linesPerPage: 12
         }
+        // React Skeleton
+        setIsLoading(true);
         makeRequest({url: '/products', params})
-        .then(response => setProductsResponse(response.data));
+        .then(response => setProductsResponse(response.data))
+        .finally(() => {
+            // React Skeleton
+            setIsLoading(false)
+        });
     }, []);
 
     return(
         <div className="catalog-container">
             <h1 className="catalog-title">Catálogo de produtos</h1>
             <div className="catalog-products">
-                {productsResponse?.content.map(product => (
-                    <Link to={`/products/${product.id}`} key={product.id}>
-                        <ProductCard product={product}/>
-                    </Link>
-                ))}
+                {isLoading ? <ProductCardLoader /> : (
+                    productsResponse?.content.map(product => (
+                        <Link to={`/products/${product.id}`} key={product.id}>
+                            <ProductCard product={product}/>
+                        </Link>
+                        ))
+                )}
             </div>
         </div>
 );
