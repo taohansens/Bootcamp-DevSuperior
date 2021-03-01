67d37de14bf29538f0b82f77d789b5989aba6fe4
Tao Hansen <taohansens@gmail.com>
2021-03-01 08:55:01 -0300

    Nesting Admin

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
 import Navbar from './components/Navbar';
+import './styles.scss'
 
 const Admin = () => (
-    <div>
+    <div className="admin-container">
         <Navbar />
+        <div className="admin-content">
+            <Switch>
+                <Route path="/admin/products">
+                    <h1>Produtos</h1>
+                </Route>
+                <Route path="/admin/categories">
+                    <h1>Categorias</h1>
+                </Route>
+                <Route path="/admin/users">
+                    <h1>Usuarios</h1>
+                </Route>
+            </Switch>
+        </div>
     </div>
 );
 
